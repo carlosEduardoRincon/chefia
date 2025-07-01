acc487040b3e8879cd13077ec9bee20c88595e70
carlosEduardoRincon <carloscaca147@gmail.com>
2025-06-30 21:52:32 -0300

    feat - estrutura do projeto

---
+++ src/main/java/com/chefia/ChefiaApplication.java
@@ -0,0 +1,13 @@
+package com.chefia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChefiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChefiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chefia/ChefiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chefia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChefiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
