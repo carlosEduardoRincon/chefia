26d53d9ded37fd77c171caedb13540edb53b0864
carlosEduardoRincon <carloscaca147@gmail.com>
2025-08-04 20:32:00 -0300

    fix: removendo import desncessário

--- src/main/java/com/chefia/repositories/UserRepository.java
+++ src/main/java/com/chefia/repositories/UserRepository.java
@@ -4,8 +4,6 @@ import com.chefia.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
     UserDetails findByLogin(String login);
 }
