cffd03f5fe85c8c786164d2b4c5d83c4e7113ef8
carlosEduardoRincon <carloscaca147@gmail.com>
2025-10-06 15:50:55 -0300

    fix: ajuste de anotação faltante

--- src/main/java/com/chefia/infra/web/AddressApiController.java
+++ src/main/java/com/chefia/infra/web/AddressApiController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
+@RestController
 @AllArgsConstructor
 public class AddressApiController implements AddressApi {
 
